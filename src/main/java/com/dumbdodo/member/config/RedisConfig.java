cc7098955df1f51641d388c027203a5a736d85d9
lakshit1307 <lakshit1307@gmail.com>
2018-11-17 21:49:40 +0530

    docker changes to start all the relevant servers required for the application.
    If the application is also to be started along with the servers, uncomment the web build code in docker-compose.yml

--- src/main/java/com/dumbdodo/member/config/RedisConfig.java
+++ src/main/java/com/dumbdodo/member/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.dumbdodo.member.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -11,10 +13,21 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Autowired
+    private JedisConnectionFactory jedisConnectionFactory;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory jedisConFactory
                 = new JedisConnectionFactory();
+        jedisConFactory.setHostName(redisHostName);
+        jedisConFactory.setPort(redisPort);
         return jedisConFactory;
     }
 
@@ -27,7 +40,8 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-        template.setConnectionFactory(jedisConnectionFactory());
+//        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(jedisConnectionFactory);
         template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return template;
     }
